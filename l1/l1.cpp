3c886bea53f149c85519c10594eb60b2d90a6616
Dikobrazzzec <zasxcd1307@mail.ru>
2020-09-25 18:42:07 +0300

    SNWC

---
+++ l1/l1.cpp
@@ -0,0 +1,17 @@
+﻿#include <Windows.h>
+#include <wincrypt.h>
+#include <iostream>
+
+using namespace std;
+
+BOOL WINAPI CryptGenRandom(_In_ HCRYPTPROV hProv, _In_ DWORD dwLen, _Inout_ BYTE* pbBuffer);
+
+int main()
+{ 
+	HCRYPTPROV hCrypt = 0;
+	const DWORD dwLength = 5;
+	BYTE pbBuffer[dwLength] = {};
+	//BYTE* ptr = &value;
+	CryptGenRandom(hCrypt, 16, pbBuffer);
+	return 0;
+}
