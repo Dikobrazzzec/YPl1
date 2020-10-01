80882894174ac95b4b19b1d128fe202d88850e84
Dikobrazzzec <zasxcd1307@mail.ru>
2020-10-01 14:27:00 +0300

    final

--- l1/l1.cpp
+++ l1/l1.cpp
@@ -6,12 +6,20 @@ using namespace std;
 
 int main()
 { 
-	BYTE value[10];
+	BYTE value[100];
 	BYTE* ptr = &value[0];
+	int QuanBytes;
+	cout <<" Enter quantity  of bytes" << endl;
+	cin >> QuanBytes;
+	cout << endl;
+	BOOL q;
 	HCRYPTPROV a;
 	//HCRYPTPROV* FParametr = &a;
 	CryptAcquireContext(&a,NULL ,NULL,PROV_RSA_FULL, CRYPT_VERIFYCONTEXT);
-	CryptGenRandom(a, 5, ptr);
-	cout << *ptr;
+	CryptGenRandom(a, QuanBytes, value);	
+	for (int i = 0; i < QuanBytes; i++) {
+		cout << i << '.' << value[i] << endl;
+	}
+	cout << endl;
 	return 0;
 }
